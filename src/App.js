6ce6de8f054714c519acec4a3c3ac642d9725cd0
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-30 03:01:22 -0600

    created DataLayer and reducer video: 2:00:55

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from './Login'
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player'
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -14,16 +18,18 @@ function App() {
 
     if(_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log(user);
+      });
     }
   }, [])
   return (
     <div className="app">
       {
-        token ? (
-          <h1>I am Logged In</h1>
-        ) : (
-        <Login/>
-        )
+        token ? (<Player/>) : (<Login/>)
       }
     </div>
   );
