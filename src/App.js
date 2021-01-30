86fe8a4dd728202b80d512ced6e55627745f1ee9
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-30 02:21:18 -0600

    completed logic to display logged in message in app.js after logging in

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from './Login'
 import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
 
-    const token = hash.access_token;
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+    }
   }, [])
   return (
     <div className="app">
-      <Login/>
+      {
+        token ? (
+          <h1>I am Logged In</h1>
+        ) : (
+        <Login/>
+        )
+      }
     </div>
   );
 }
