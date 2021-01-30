17a9b154ccb7023f39855fec6ccc7e1e5e81b183
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-30 03:46:08 -0600

    bug: App.js line 13user is causing problems. start there to debug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
-import Login from './Login'
-import { getTokenFromUrl } from './spotify';
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
-import Player from './Player'
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -22,10 +24,16 @@ function App() {
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-        console.log(user);
+
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        });
       });
     }
-  }, [])
+  }, []);
+
+  console.log(user);
   return (
     <div className="app">
       {
