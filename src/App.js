f3806bea7f1c92067f2c64577cd76bac1f9ee220
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-30 18:23:08 -0600

    setup banner portion of discover weekly display with responsive text from api

--- src/App.js
+++ src/App.js
@@ -41,6 +41,13 @@ function App() {
         dispatch({
           type: "SET_PLAYLISTS",
           playlists: playlists,
+        });
+      });
+
+      spotify.getPlaylist('37i9dQZEVXcShxPnIhnbRh').then(response => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
         })
       })
     }
